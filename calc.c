d81afdbb3fc3d0640c786b3c5d0645aeaa0df4d8
Francisco <fp226766@gmail.com>
2025-04-08 09:43:53 +0100

    Add calculator and bitwise operations functions in calc.c

---
+++ calc.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "shell.h"
+#define epsilon 1.0e-16
+
+
+void calc(char *value1, char *op, char *value2) {
+    double num1 = atof(value1);
+    double num2 = atof(value2);
+    double resultado;
+
+    if (op == NULL || value1 == NULL || value2 == NULL) {
+        printf("Erro: argumentos inválidos.\n");
+        return;
+    }
+
+    switch (op[0]) {
+        case '+':
+            resultado = num1 + num2;
+            printf("Resultado calc %.3f + %.3f = %.3f\n", num1, num2, resultado);
+            break;
+        case '-':
+            resultado = num1 - num2;
+            printf("Resultado calc %.3f - %.3f = %.3f\n", num1, num2, resultado);
+            break;
+        case '*':
+            resultado = num1 * num2;
+            printf("Resultado calc %.3f * %.3f = %.3f\n", num1, num2, resultado);
+            break;
+        case '/':
+            if (fabs(num2) < epsilon) {
+                printf("Erro: divisão por zero.\n");
+            } else {
+                resultado = num1 / num2;
+                printf("Resultado calc %.3f / %.3f = %.3f\n", num1, num2, resultado);
+            }
+            break;
+        case '^':
+            resultado = pow(num1, num2);
+            printf("Resultado calc %.3f ^ %.3f = %.3f\n", num1, num2, resultado);
+            break;
+        default:
+            printf("Erro: operador desconhecido '%s'\n", op);
+    }
+}
+
+void bits(char *op1, char *op, char *op2) {
+    if (op1 == NULL || op == NULL) {
+        printf("Erro: argumentos inválidos.\n");
+        return;
+    }
+
+    unsigned short num1 = (unsigned short)atoi(op1);
+    unsigned short num2 = op2 ? (unsigned short)atoi(op2) : 0;
+    unsigned short resultado;
+
+    switch (op[0]) {
+        case '&':
+            resultado = num1 & num2;
+            printf("Resultado bits %hu & %hu = %hu\n", num1, num2, resultado);
+            break;
+        case '|':
+            resultado = num1 | num2;
+            printf("Resultado bits %hu | %hu = %hu\n", num1, num2, resultado);
+            break;
+        case '^':
+            resultado = num1 ^ num2;
+            printf("Resultado bits %hu ^ %hu = %hu\n", num1, num2, resultado);
+            break;
+        case '~':
+            // operador ~ é unário, só aplica a num1
+            printf("Resultado bits ~%hu = %hu\n", num1, (unsigned short)(~num1));
+            break;
+        default:
+            printf("Operador inválido: %s\n", op);
+    }
+}
