6db543d9e6be74ad828bd501472ec105e2d642a7
Francisco <fp226766@gmail.com>
2025-06-02 23:57:11 +0100

    Refactor execute and parse functions for improved readability and functionality

--- execute.c
+++ execute.c
@@ -1,82 +1,112 @@
 #include "shell.h"
 
-int ultimo ( int *numargs, char **args )
-{
-  if ( args[*numargs-1][0]=='&') {
-    *numargs=*numargs-1;
-    args[*numargs]=NULL ;
-    return BG;
-  }
-  return FG;
+int ultimo(int *numargs, char **args) {
+    if (*numargs > 0 && args[*numargs - 1][0] == '&') {
+        (*numargs)--;
+        args[*numargs] = NULL;
+        return BG;
+    }
+    return FG;
 }
 
-int containsPipe (int numArgs, char **args) {
-  for (int i = 0; i < numArgs; i++) {
-    if (args[i][0] == '|')
-      return i;
-  }
-  return -1;
+int containsPipe(int numArgs, char **args) {
+    for (int i = 0; i < numArgs; i++) {
+        if (strcmp(args[i], "|") == 0)
+            return i;
+    }
+    return -1;
 }
 
-void execute (int numargs, char **args)
-{
-  int code = ultimo(&numargs, args);
-  int pid, status;
-
-  int pipeIndex = containsPipe(numargs, args);
-
-  if ((pid = fork ()) < 0) {
-    perror ("fork");
-    exit (1);
-  }
-
-  if (pid == 0) { // processo filho do shell
+void execute_rec(int numargs, char **args, int input_fd) {
+    int pipeIndex = containsPipe(numargs, args);
 
     if (pipeIndex == -1) {
-      numargs = redirects(numargs, args);
-      execvp(*args, args);
-      perror(*args);
-      exit(1);
+        // Último comando
+        if (input_fd != STDIN_FILENO) {
+            dup2(input_fd, STDIN_FILENO);
+            close(input_fd);
+        }
+
+        args[numargs] = NULL;  
+        numargs = redirects(numargs, args);
+
+        execvp(args[0], args);
+        perror(args[0]);
+        exit(1);
     }
 
-    // PIPE DETECTADO
+    // Separar comandos à esquerda e direita do pipe
+    args[pipeIndex] = NULL;
+    int numargs1 = pipeIndex;
+    char **args1 = args;
+    int numargs2 = numargs - pipeIndex - 1;
+    char **args2_raw = &args[pipeIndex + 1];
+
+    // Copiar args2 para novo array com NULL
+    char *args2[64];  // ajusta o tamanho conforme o necessário
+    for (int i = 0; i < numargs2; i++) {
+        args2[i] = args2_raw[i];
+    }
+    args2[numargs2] = NULL;
+
+    args1[numargs1] = NULL;
 
     int fd[2];
-    pipe(fd);
-
-    args[pipeIndex] = NULL; // cortar o símbolo do pipe
-
-    pid_t pid_neto = fork();
-
-    if (pid_neto == 0) {
-      // Processo NETO: comando antes do pipe
-      close(fd[0]); 
-      dup2(fd[1], STDOUT_FILENO);
-      close(fd[1]);
-      numargs = redirects(pipeIndex, args);
-      execvp(args[0], args);
-      perror("execvp neto");
-      exit(1);
-    } else {
-      // Processo FILHO: comando depois do pipe
-      close(fd[1]); 
-      dup2(fd[0], STDIN_FILENO);
-      close(fd[0]);
-      char **args2 = &args[pipeIndex + 1];
-      int numargs2 = numargs - pipeIndex - 1;
-      numargs2 = redirects(numargs2, args2); 
-      execvp(args2[0], args2);
-      perror("execvp filho");
-      exit(1);
+    if (pipe(fd) == -1) {
+        perror("pipe");
+        exit(1);
     }
 
-  } else {
+    pid_t pid = fork();
+    if (pid == -1) {
+        perror("fork");
+        exit(1);
+    }
+
+    if (pid == 0) {
+        // Filho: executa comando à esquerda
+        dup2(fd[1], STDOUT_FILENO);
+        if (input_fd != STDIN_FILENO) {
+            dup2(input_fd, STDIN_FILENO);
+            close(input_fd);
+        }
+
+        close(fd[0]);
+        close(fd[1]);
+
+        numargs1 = redirects(numargs1, args1);
+
+        execvp(args1[0], args1);
+        perror(args1[0]);
+        exit(1);
+    }
+
+    // Processo pai continua recursivamente
+    close(fd[1]);
+    if (input_fd != STDIN_FILENO)
+        close(input_fd);
+
+    execute_rec(numargs2, args2, fd[0]);
+}
+
+void execute(int numargs, char **args) {
+    int code = ultimo(&numargs, args);
+    pid_t pid = fork();
+    int status;
+
+    if (pid < 0) {
+        perror("fork");
+        exit(1);
+    }
+
+    if (pid == 0) {
+        execute_rec(numargs, args, STDIN_FILENO);
+        exit(1);
+    }
+
     if (code == FG) {
-      while (wait (&status) != pid);
+        while (wait(&status) != pid);
     } else {
-      printf("[BG] Processo iniciado com PID: %d\n", pid);
+        printf("[BG] Processo iniciado com PID: %d\n", pid);
     }
-  }
-
-  return;
 }
--- parse.c
+++ parse.c
@@ -1,27 +1,20 @@
 #include "shell.h"
 
-/*
-    parse . particiona o comando (armazenado em ptrLinha) em argumentos
-    exemplo ptrLinha -> "ls  -l /etc/passwd & "
-    O string contem caracteres e espaços e é terminado com um \0
-*/
-
-int parse (char *buf, char **args)
+int parse(char *buf, char **args)
 {
   int cnt = 0;
-  while ('\0' != *buf)
+  while (*buf != '\0')
+  {
+    while (*buf == ' ' || *buf == '\t')
+      *buf++ = '\0';
+    if (*buf != '\0')
     {
-      /* saltar whitespace. Substituimos whitespace por um \0 */
-      while (isspace ((unsigned char) *buf))
-        *buf++ = '\0';
-
-      *args++ = buf;/* salvaguarda argumento */
+      *args++ = buf;
       cnt++;
-
-      /* salta sobre os caracteres do argumento ou encontrar o fim da linha*/
-      while ((*buf != '\0') && (!isspace ((unsigned char) *buf)))
-        buf++;
     }
-  *args = (char *) NULL;/* Inserir o ultimo argumento igual a NULL */
+    while (*buf != '\0' && *buf != ' ' && *buf != '\t')
+      buf++;
+  }
+  *args = NULL;
   return cnt;
 }
